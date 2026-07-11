9ca1d9403b2649e0738462d2f34962c8ed2024b7
Ikram697 <z.gmiza@gmail.com>
2026-07-11 18:04:15 +0200

    Création de l'evenement changement de texte en cliquant sur le bouton et apparition d'une phrase. Et changement du nom de la source JS.

--- Javascript.js
+++ Javascript.js
@@ -1,2 +1,7 @@
 const button = document.getElementById("Bouton_TP_Javascript");
 const p = document.getElementById("Premier_Paragraphe_TP_Javascript");
+
+button.addEventListener("click", function() {
+    button.innerHTML = "Vous avez cliqué sur le bouton !";
+    p.innerHTML = "Je viens de manipuler le DOM";
+});
