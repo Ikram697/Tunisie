aef7e9b19a3a85d7450a4d6c635ea80a7d48a3fc
Ikram697 <z.gmiza@gmail.com>
2026-07-20 14:53:14 +0200

    Création du bouton dans JS et affichage d'une phrase en dessous. Création également d'un bouton dans Index avec affichage du nombre de like.

--- Javascript.js
+++ Javascript.js
@@ -1,7 +1,7 @@
-const button = document.getElementById("Bouton_TP_Javascript");
-const p = document.getElementById("Premier_Paragraphe_TP_Javascript");
+const button = document.getElementById("Bouton");
+const p = document.getElementById("Phrase_paragraphe");
 
-button.addEventListener("click", function() {
-    button.innerHTML = "Vous avez cliqué sur le bouton !";
-    p.innerHTML = "Je viens de manipuler le DOM";
+button.addEventListener("click",function() {
+    button.innerHTML = "Vous m'avez cliqué.";
+    p.innerHTML = "Vous maitrisez le DOM.";
 });
---
+++ Javascript2.js
@@ -0,0 +1,10 @@
+const button_2 = document.getElementById("Bouton_photo");
+const affichage_like = document.getElementById("augmentation_like");
+
+let Score = 0;
+
+button_2.addEventListener ("click",function() {
+    Score = Score + 1;
+    affichage_like.innerHTML = Score; 
+
+});
