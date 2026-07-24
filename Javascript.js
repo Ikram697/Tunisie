2b929e96af21594225718ecab1f9814af77f1494
Ikram697 <z.gmiza@gmail.com>
2026-07-24 15:01:27 +0200

    Création d'une image avec son titre et un bouton qui lorsqu'on clique dessus modifie l'image et le titre.

--- Javascript.js
+++ Javascript.js
@@ -5,3 +5,12 @@ button.addEventListener("click",function() {
     button.innerHTML = "Vous m'avez cliqué.";
     p.innerHTML = "Vous maitrisez le DOM.";
 });
+
+const bouton_decollage = document.getElementById("Bouton_decollage");
+const titre_Aeroport = document.getElementById("Aeroport");
+const image_Aeroport = document.getElementById("Image_Aeroport");
+
+bouton_decollage.addEventListener("click",function() {
+    titre_Aeroport.innerHTML = "Tunis";
+    image_Aeroport.src = "Aeroport_Tunis_Carthage.jpg";
+});
