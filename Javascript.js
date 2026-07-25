d93dd03ec57988aca38c931ca948274d4bcd30aa
Ikram697 <z.gmiza@gmail.com>
2026-07-25 17:31:36 +0200

    Création des constantes des dépenses en euros et des dépenses en dinards ainsi que de la constante du bouton conversion

--- Javascript.js
+++ Javascript.js
@@ -14,3 +14,19 @@ bouton_decollage.addEventListener("click",function() {
     titre_Aeroport.innerHTML = "Tunis";
     image_Aeroport.src = "Aeroport_Tunis_Carthage.jpg";
 });
+
+const billet_avion_euros = document.getElementById("billet_avion_en_euros");
+const restaurants_euros = document.getElementById("depenses_restaurants_en_euros");
+const courses_euros = document.getElementById("courses_en_euros");
+const activites_euros = document.getElementById("activites_en_euros");
+const souvenirs_euros = document.getElementById("souvenirs_en_euros");
+const total_euros = document.getElementById("total_en_euros");
+
+const billet_avion_dinards = document.getElementById("billet_avion_en_dinards");
+const restaurants_dinards = document.getElementById("depenses_restaurants_en_dinards");
+const courses_dinards = document.getElementById("courses_en_dinards");
+const activites_dinards = document.getElementById("activites_en_dinards");
+const souvenirs_dinards = document.getElementById("souvenirs_en_dinards");
+const total_dinards = document.getElementById("total_en_dinards");
+
+const bouton_conversion = document.getElementById("Bouton_conversion");
