a343d2a6da6acd806a61e63cbccccb390259b487
Ikram697 <z.gmiza@gmail.com>
2026-08-03 14:42:48 +0200

    Création de la conversion des autres enregistrement de l'euro en dinards du tableau en cliquant sur le bouton conversion.

--- Javascript.js
+++ Javascript.js
@@ -30,3 +30,39 @@ const souvenirs_dinards = document.getElementById("souvenirs_en_dinards");
 const total_dinards = document.getElementById("total_en_dinards");
 
 const bouton_conversion = document.getElementById("Bouton_conversion");
+
+let taux_de_change = 3.3;
+
+let billet_avion_texte = billet_avion_euros.innerText;
+let billet_avion_nombre = parseFloat (billet_avion_texte);
+let billet_avion_convertis = billet_avion_nombre * taux_de_change;
+
+let restaurants_texte = restaurants_euros.innerText;
+let restaurant_nombre = parseFloat (restaurants_texte);
+let restaurant_convertis = restaurant_nombre * taux_de_change;
+
+let courses_texte = courses_euros.innerText;
+let courses_nombre = parseFloat (courses_texte);
+let courses_convertis = courses_nombre * taux_de_change;
+
+let activites_texte = activites_euros.innerText;
+let activites_nombre = parseFloat (activites_texte);
+let activites_convertis = activites_nombre * taux_de_change;
+
+let souvenirs_texte = souvenirs_euros.innerText;
+let souvenirs_nombre = parseFloat (souvenirs_texte);
+let souvenirs_convertis = souvenirs_nombre * taux_de_change;
+
+let total_texte = total_euros.innerText;
+let total_nombre = parseFloat(total_texte);
+let total_convertis = total_texte * taux_de_change;
+
+
+bouton_conversion.addEventListener("click",function() {
+    billet_avion_dinards.innerHTML = billet_avion_convertis;
+    restaurants_dinards.innerHTML = restaurant_convertis;
+    courses_dinards.innerHTML = courses_convertis;
+    activites_dinards.innerHTML = activites_convertis;
+    souvenirs_dinards.innerHTML = souvenirs_convertis;
+    total_dinards.innerHTML = total_convertis;
+});
